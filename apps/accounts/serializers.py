c9903cef0dbe02c3967c6c93e214bde278cc1d37
Mohammed Rishad <mohammedrishad.vk@gmail.com>
2022-04-23 08:45:58 +0530

    Initial commit

---
+++ apps/accounts/serializers.py
@@ -0,0 +1,25 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("id", "username", "password", "email", "first_name", "last_name")
+        read_only_fields = ("id",)
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+        )
+        user.set_password(validated_data["password"])
+        user.save()
+        return user
---
+++ apps/accounts/views.py
@@ -0,0 +1,17 @@
+from django.contrib.auth import get_user_model
+
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import AllowAny
+
+from apps.accounts.serializers import UserSerializer
+
+User = get_user_model()
+
+
+class RegisterUserView(CreateAPIView):
+    """
+        User registration endpoint
+    """
+    model = User
+    permission_classes = [AllowAny]
+    serializer_class = UserSerializer
